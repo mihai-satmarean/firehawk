24a47937c4a767415f925f275657e6a7e007f9e2
Andrew Graham <queglay@gmail.com>
2020-03-07 09:48:20 +1030

    echo vault key

--- scripts/ci-test-decrypt.sh
+++ scripts/ci-test-decrypt.sh
@@ -2,8 +2,8 @@
 # This script should be executed from the firehawk folder
 . ./scripts/exit_test.sh
 source ./update_vars.sh --dev --vagrant; exit_test
-echo $testsecret
-echo $vault_key
+echo "testsecret $testsecret"
+echo "vault_id $vault_key"
 result=$(./scripts/ansible-encrypt.sh --vault-id $vault_key --decrypt $testsecret)
 echo $result
 if [[ "$result" != "this is a test secret" ]]; then exit 1; fi
