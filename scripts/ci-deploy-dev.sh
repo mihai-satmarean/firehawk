cfff974349e904214926e1532b71f40bfca2500d
Andrew Graham <queglay@gmail.com>
2020-03-07 01:23:11 +1030

    import to abs path function

--- scripts/ci-deploy-dev.sh
+++ scripts/ci-deploy-dev.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+function to_abs_path {
+    local target="$1"
+    if [ "$target" == "." ]; then
+        echo "$(pwd)"
+    elif [ "$target" == ".." ]; then
+        echo "$(dirname "$(pwd)")"
+    else
+        echo "$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
+    fi
+}
+
 config_override=$(to_abs_path $TF_VAR_firehawk_path/../secrets/config-override-$TF_VAR_envtier) # ...Config Override path $config_override.
 echo "config_override path- $config_override"
 sed -i 's/^TF_VAR_enable_vpc=.*$/TF_VAR_enable_vpc=true/' $config_override # ...Enable the vpc.
