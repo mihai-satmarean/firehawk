da47b236cb41951b7d57bf402165a4c5ef113de0
Andrew Graham <queglay@gmail.com>
2020-03-07 18:07:23 +1030

    script dir may be invalid

--- scripts/firehawk-ssh.sh
+++ scripts/firehawk-ssh.sh
@@ -7,8 +7,11 @@ echo "tier $3"
 # This is the directory of the current script
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SCRIPTDIR=$(to_abs_path $SCRIPTDIR)
-printf "\n...checking scripts directory at $SCRIPTDIR\n\n"
+printf "\n...checking scripts directory at: $SCRIPTDIR\n\n"
 # source an exit test to bail if non zero exit code is produced.
-. $SCRIPTDIR/exit_test.sh
+echo "TF_VAR_firehawk_path: $TF_VAR_firehawk_path"
+ls $TF_VAR_firehawk_path/scripts
+
+. $TF_VAR_firehawk_path/scripts/exit_test.sh
 
 ssh deployuser@$1 firehawksecret="$firehawksecret" -p $2 -i ../secrets/keys/ansible_control_private_key -o StrictHostKeyChecking=no -t "/deployuser/scripts/init-firehawk.sh $3"; exit_test
